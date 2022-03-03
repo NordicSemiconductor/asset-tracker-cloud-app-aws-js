2d85afa28330b8854ab05a7f253716c93a584642
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-03-03 11:44:42 +0100

    fix: add margin to outer box so bottom is easier to reach

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,7 @@ import type { FunctionComponent, PropsWithChildren } from 'react'
 export const Main: FunctionComponent<PropsWithChildren<unknown>> = ({
 	children,
 }) => (
-	<main className="container">
+	<main className="container mb-4">
 		<div className="row justify-content-center">
 			<div className="col-md-10 col-lg-8 col-xl-6">{children}</div>
 		</div>

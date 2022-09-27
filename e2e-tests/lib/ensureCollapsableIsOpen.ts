8f7d2fd20de110f4518af417902b5f4211c0d837
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-09-27 13:51:16 +0200

    style: add return type

--- e2e-tests/lib/ensureCollapsableIsOpen.ts
+++ e2e-tests/lib/ensureCollapsableIsOpen.ts
@@ -3,17 +3,19 @@ import { expect, Page } from '@playwright/test'
 /**
  * Helper function that ensures the collapsable with the id is open.
  */
-export const ensureCollapsableIsOpen = (page: Page) => async (id: string) => {
-	const collapsibleHeader = page.locator(`[id="${id}"] >> header`).first()
+export const ensureCollapsableIsOpen =
+	(page: Page) =>
+	async (id: string): Promise<void> => {
+		const collapsibleHeader = page.locator(`[id="${id}"] >> header`).first()
 
-	const expanded = await collapsibleHeader.getAttribute('aria-expanded')
+		const expanded = await collapsibleHeader.getAttribute('aria-expanded')
 
-	if (expanded === 'false') {
-		await collapsibleHeader.click()
+		if (expanded === 'false') {
+			await collapsibleHeader.click()
+		}
+
+		expect(
+			await collapsibleHeader.getAttribute('aria-expanded'),
+			`Collapsable ${id} is not open.`,
+		).toEqual('true')
 	}
-
-	expect(
-		await collapsibleHeader.getAttribute('aria-expanded'),
-		`Collapsable ${id} is not open.`,
-	).toEqual('true')
-}

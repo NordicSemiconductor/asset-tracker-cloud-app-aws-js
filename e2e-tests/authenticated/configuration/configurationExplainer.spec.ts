31dcc55711ad973061b3a940f07b7f86177cc445
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-09-29 13:42:40 +0200

    feat(explainer): round raw values

--- e2e-tests/authenticated/configuration/configurationExplainer.spec.ts
+++ e2e-tests/authenticated/configuration/configurationExplainer.spec.ts
@@ -12,7 +12,7 @@ export const explainDuration = (value: number): string => {
 	if (isNaN(value)) return '... click here to fill input'
 	return `${formatDuration(
 		intervalToDuration({ start: 0, end: value * 1000 }),
-	)} (${value} seconds)`
+	)} (${Math.round(value)} seconds)`
 }
 
 test.use({
